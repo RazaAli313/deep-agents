85be9921096090cb06c009f1b745bfa472ac26df
RazaAli313 <razaalipk313@gmail.com>
2026-02-18 16:40:58 +0500

    subagents in deepagents

--- main.py
+++ main.py
@@ -53,19 +53,34 @@ def Web_Search(
         query=query, max_results=max_results, include_raw_content=include_raw_content, topic=topic
     )
 
+def get_name()->str:
+    """
+    This  is a tool function  that returns my name
+    """
+    return "Syed Muhammad Raza Ali Zaidi"
+
+name_agent={
+    "name":"Name Agent",
+    "description": "This is a agent that tells me my name",
+    "system_prompt":"You are a name agent that returns my name using tool get_name",
+    "tools":[get_name],
+    "model":client
+
+}
 
 agent=create_deep_agent(
     model=client,
     system_prompt=
     """
-    You are a helpul assistant that returns the web search result using your tool tavily about the given topic by the user,remember only use the tavily tool for task of general knowledge don;t use your internal knowledge
+    You are a helpul assistant that returns the web search result using your tool tavily about the given topic by the user,remember only use the tavily tool for task of general knowledge don't use your internal knowledge,use sub agent name_agent if user asks my name
     """,
     tools=[Web_Search],
+    subagents=[name_agent],
     middleware=[log_tool_call]
 
 )
 
-result=agent.invoke({"messages":[{"role":"user","content":"Hi what is the capital of Pakistan???"}]})
+result=agent.invoke({"messages":[{"role":"user","content":"what is my name???"}]})
 
 # print(result)
 print(result["messages"][-1].content)
